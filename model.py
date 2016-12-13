ef9bb227d1b0c28c8bd1b03989930566d139de46
rerafyaw <anderson_nov@hotmail.com>
2016-12-13 17:55:04 -0500

    integrated hysteresis into model, early version

--- model.py
+++ model.py
@@ -3,10 +3,21 @@ import numpy as np
 import scipy
 from scipy.integrate import quad as integrate
 from matplotlib import pyplot as plt
+from scipy.interpolate import interp1d
 
 pi = np.pi
-
-mu0 = 4e-7 * pi
+# Permittivity of free space
+mu0 = 4e-7 * pi # H/m
+# Scale of mag field strength
+a = 470 # A/m
+# Mean field param
+alpha = 9.38e-4
+# Weighting of anhysteric vs irreversible magnetization (1 removes Mrev)
+c = 0.0889
+# Sizing of hysteresis
+k = 483 # A/m
+# Saturation magnetization
+Ms = 1.48e6 # A/m
 
 def vec(*args):
     return np.atleast_2d(args).T
@@ -38,6 +49,74 @@ class Levitron(Magnet):
     def field(self, current, position):
         # depends on H-Field
         pass
+    def hysteresis(self, magFieldStr):
+        delta = [0]
+        Man = [0]
+        dMirrdH = [0]
+        Mirr = [0]
+        M = [0]
+        H = [0]
+
+        # Tracks change of ext field H (permeance) to magnetization
+        DeltaH = 1
+        # Values below 2 reflect a hard ferromagnet - (high coercivity, lower saturation mag)
+        # Values above 4 reflect a soft ferromagnet - (low coercivity, higher sat mag)
+        Nfirst = 1250 # initial magnetization curve range (DO NOT CHANGE as it is basically a vertical axis offset)
+        Ndown = 2500
+        Nup = 2500
+        # switch to magField when complete
+        val = 523 # sample value of H applied field (kA/m) (actual x 10^(-3))
+
+        for i in range(Nfirst):
+            H.append(H[i] + DeltaH)
+
+        for i in range(Ndown):
+            H.append(H[-1] - DeltaH)
+
+        for i in range(Nup):
+            H.append(H[-1] + DeltaH)
+
+        for i in range(len(H) -1):
+            if H[i + 1] > H[i]: # determines the direction of movement
+                delta.append(1)
+            else:
+                delta.append(-1)
+
+        def L(x):
+            return (np.cosh(x) / np.sinh(x)) - (1 / x)
+
+        for i in range(Nfirst + Ndown + Nup):
+            Man.append(Ms * (1 / np.tanh((H[i + 1] + alpha * M[i]) / a) - a / (H[i + 1] + alpha * M[i])))
+            dMirrdH.append((Man[i+1] - M[i]) / (k * delta[i+1] - alpha * (Man[i + 1] - M[i])))
+            Mirr.append(Mirr[i] + dMirrdH[i + 1] * (H[i+1] - H[i]))
+            M.append(c * Man[i + 1] + (1 - c) * Mirr[i + 1])
+            if (H[i] == val): # only detects integers/whole numbers
+                B_field = H[i]*mu0*(1000.0)
+                data_x = [float(H[i])]
+                data_y = [float(M[i])]
+                print data_y
+                print str(B_field) + ' Teslas'
+                plt.plot(data_x, data_y, 'or')
+
+        plt.xlabel('Applied magnetic field H (A/m)')
+        plt.ylabel('Magnetization M (MA/m)')
+        plt.plot(H, M)
+        print "magnetic saturation", max(M)/pow(10,6)
+
+        # reducing anhysteric magnetization range to upper/lower curve values
+        startAn = Nfirst + Ndown
+        endAn = Nfirst + Nup
+        end = startAn + Ndown
+        M_up = M[Nfirst:endAn]
+        # for polyfitting ()
+        #H_an = H[Nfirst:endAn]
+
+        # Interpolation curve - added in v1.3
+        H_an2 = H[startAn:end] #FLIPPED IT!
+        M_up2 = M_up[::-1]
+        polation = interp1d(H_an2, M_up2)
+        plt.plot(H_an, p(H_an),'o', H_an2, polation(H_an2),'--')
+        plt.show()
 
 class Solenoid(Magnet):
     def __init__(self, radius, length, loops, pose=None):
